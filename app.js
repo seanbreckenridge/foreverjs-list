160b452a310b2b159a6a72db60d8f068c12b4826
Sean Breckenridge <seanbrecke@gmail.com>
2020-03-20 18:27:42 -0700

    use custom date handler for morgan
    previously dates would be in UTC time
    make sure it uses local timezone

--- app.js
+++ app.js
@@ -34,7 +34,38 @@ const app = express();
 
 // middleware
 // logging
-app.use(morgan('short'));
+
+// helper function to use local timezone in morgan log timestamp
+// from https://github.com/expressjs/morgan/issues/115#issuecomment-239399047
+morgan.token('realclfdate', function(req, res) {
+  const clfmonth = [
+    'Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun',
+    'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec',
+  ];
+  const pad2 = function(num) {
+    const str = String(num);
+
+    return (str.length === 1 ? '0' : '') +
+    str;
+  };
+  const dateTime = new Date();
+  const date = dateTime.getDate();
+  const hour = dateTime.getHours();
+  const mins = dateTime.getMinutes();
+  const secs = dateTime.getSeconds();
+  const year = dateTime.getFullYear();
+  let timezoneofset = dateTime.getTimezoneOffset();
+  const sign = timezoneofset > 0 ? '-' : '+';
+  timezoneofset = parseInt(Math.abs(timezoneofset)/60);
+  const month = clfmonth[dateTime.getUTCMonth()];
+
+  return pad2(date) + '/' + month + '/' + year +
+    ':' + pad2(hour) + ':' + pad2(mins) + ':' + pad2(secs) +
+    ' '+sign+pad2(timezoneofset)+'00';
+});
+
+app.use(morgan(':remote-addr - :remote-user [:realclfdate] ":method :url HTTP/:http-version" :status :res[content-length] ":referrer" ":user-agent"'));
+
 app.use(helmet());
 
 
